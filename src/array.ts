492a20e0d270cf2fc8da41cfb8d11c8baf322967
kato.jun <kato.jun@classmethod.jp>
2021-11-02 12:12:45 +0900

    type array

---
+++ src/array.ts
@@ -0,0 +1,11 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+let strings: string[] = ['Tokyo', 'Osaka', 'Fukuoka'];
+
+let nijigenHairetsu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairetu: (string | number | boolean)[] = [1, false, 'Japan'];
