1bc6e6660d43a06497d9dcdb5ac29ad3a8d821f5
kato.jun <kato.jun@classmethod.jp>
2021-11-02 15:44:27 +0900

    literal

---
+++ src/literal.ts
@@ -0,0 +1,12 @@
+export {};
+
+// 文字列のリテラル型
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土' = '日';
+dayOfTheWeek = '水';
+
+// 数値のリテラル型
+let month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+month = 12;
+
+// booleanのリテラル型
+let TRUE: true = true;
