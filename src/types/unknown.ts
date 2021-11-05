1e54ef47e7c2ac777558b33cfa87c73d2b93d6e2
kato.jun <kato.jun@classmethod.jp>
2021-11-05 09:49:14 +0900

    unknown型に対する説明を記載

--- src/types/unknown.ts
+++ src/types/unknown.ts
@@ -3,9 +3,11 @@ export {};
 const kansu = (): number => 43;
 
 let numberAny: any = kansu();
-let numberUnknown: unknown = kansu();
+let numberUnknown: unknown = kansu(); // unknownは型安全なany型
 
-let sumAny = numberAny + 10;
+let sumAny = numberAny + 10; // anyにnumberの10を足してもエラーにならない
+//let sumUnknown = numberUnknown + 10; // unknownにnumberの10を足そうとするとエラーになる（unknownなので数値じゃないかもしれないよ）
+
 if (typeof numberUnknown === 'number') {
   // type guard
   let sumUnknown = numberUnknown + 10;
