975a3558943df60ac3b1fac3894113c4ddb824b9
kato.jun <kato.jun@classmethod.jp>
2021-11-05 10:29:11 +0900

    コメントをupdate

--- src/types/object.ts
+++ src/types/object.ts
@@ -3,7 +3,10 @@ export {};
 let profile1: object = { name: 'kato' };
 profile1 = { age: 36 };
 
-let profile2: {
+let profile2: {} = { name: 'kato' };
+profile2 = { age: 36 };
+
+let profile3: {
   name: string;
 } = { name: 'kato' };
-profile2 = { name: 'jun' };
+profile3 = { name: 'jun' };
